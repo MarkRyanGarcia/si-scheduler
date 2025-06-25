4feadb0517f44d8908e1c9a492cb10ee8b4844ae
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-06-24 22:13:48 -0700

    Setup Dev navbar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
-import './dev-style.css'
 import App from './App.vue'
 import router from './router'
+
+if (import.meta.env.MODE === 'development') {
+    import('./dev-style.css')
+}
 
 createApp(App).use(router).mount('#app')
