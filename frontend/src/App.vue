f6f1005a761acd56dd8ce81df303c125ef126582
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-10-28 01:38:05 -0700

    initial setup

--- frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/schedules">Schedules</router-link>
-    </nav>
-    <router-view />
-</template>
